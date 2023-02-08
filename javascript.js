f0f81b61d5284d89f3dd4dca36eddd0d386b7c6f
trainboy <unanimoususer99@gmail.com>
2023-02-08 00:43:50 -0800

    create input function

--- javascript.js
+++ javascript.js
@@ -1,83 +1,67 @@
-
-//const canvas = document.querySelector('#canvas');
-
-// let column = document.getElementsByClassName('columns');
-// let row = document.getElementsByClassName('rows');
-//console.log(column,row)
-
-// function getCol(colNum) {
-//         for (j=0; j < colNum; j++) {
-//         let addCols = document.createElement('div');
-//         addCols.textContent = 'col';
-//         canvas.appendChild(addCols).className= 'columns';
-//     }
-// }
-
-
-// function getRow(rowNum) {
-//     for (i=0; i < rowNum; i++) {
-//         let addRows = document.createElement('div');
-//         addRows.textContent = 'row';
-//         canvas.appendChild(addRows).className= 'rows';
-       
-//     }
-// }
-
-// let column = document.getElementsByClassName('columns');
-// let row = document.getElementsByClassName('rows');
 let grid = document.getElementById("canvas");
+let penSwitch = document.querySelector('.pen');
+let del = document.querySelector('.erase')
+let make = document.querySelector('.enter')
+let exist = false;
 
-console.log(grid)
+// let table = document.createElement('TABLE');
+//     table.border = '1';
+//     table.style.width ='700px';
+//     table.style.height='700px';
+//     table.style.borderSpacing= 'inherit';
+//     table.style.textAlign= 'center';
+
+//     let tableBody = document.createElement('TBODY');
+//     table.appendChild(tableBody)
 
 function makeGrid() {
-    
+    let exist = true;
+    let rowValue = document.querySelector('.rowValue').value;
+    let colValue = document.querySelector('.colValue').value;
     let table = document.createElement('TABLE');
     table.border = '1';
     table.style.width ='700px';
     table.style.height='700px';
     table.style.borderSpacing= 'inherit';
     table.style.textAlign= 'center';
-
+    
     let tableBody = document.createElement('TBODY');
     table.appendChild(tableBody);
 
-    for (i = 0; i < 50; i++) {
-        let rows = document.createElement('TR');
-        tableBody.appendChild(rows);
+    for (i = 0; i < rowValue; i++) {
+        let row = document.createElement('TR');
+        tableBody.appendChild(row);
 
-        for (j = 0; j < 50; j++) {
-            let columns = document.createElement('TD');
-            columns.width = '75';
-            columns.appendChild(document.createTextNode(''));
-            rows.appendChild(columns);
-            columns.addEventListener('mouseover', ()=> {
-                columns.style.backgroundColor ='red'
-            })
-        }  
-    }
-    grid.appendChild(table);
-
-
-    // tableBody.addEventListener('click', ()=> {
-    //     tableBody.style.backgroundColor = 'red';
-    // })
-
-}
-makeGrid();
-
-function () {
-
-   
+        for (j = 0; j < colValue; j++) {
+            let col = document.createElement('TD');
+            col.width = '75';
+            col.appendChild(document.createTextNode(''));
+            row.appendChild(col);
+            col.addEventListener('mouseover', pen);
+            
+        };  
+    };
+     grid.appendChild(table);
+    //  if (exist === true) {
+    //     table.remove();
+    //  }
+     make.addEventListener('click', ()=>{
+        table.remove();
+     })
+     
 }
 
+ function pen() {
+         this.style.backgroundColor ='red';
+ };
 
-//console.log(rows)
-// let cell = document.getElementsByName('<tr>', '<td>');
-// console.log(cell)
+//function remove() {
+   // makeGrid();
+   // tableBody.remove()
+    // let tableBody = document.createElement('TBODY');
+    // tableBody.removeChild(tableBody.lastChild)
+    
+ //}
 
-//grid.addEventListener('click', fill);
-
-
-
-
+  //document.querySelector('.enter').addEventListener('submit', );
 
